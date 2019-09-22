3685e72a7847a83a2417ec30c57abf9560530b3b
Joker <987856377@qq.com>
2019-09-22 11:35:42 +0800

    Update characterEncoding.sql

--- characterEncoding.sql
+++ characterEncoding.sql
@@ -1,6 +1,9 @@
-grant all privileges on  *.* to 'user'@'localhost';
+grant all privileges on  *.* to 'user'@'localhost' WITH GRANT OPTION;
+SHOW GRANTS FOR 'user'@'localhost';
+
 ALTER USER 'user_name'@'localhost' identified with mysql_native_password by 'user_password';
 FLUSH privileges;
+
 show variables where variable_name like 'character_set_%' or variable_name like 'collation%';
 ALTER DATABASE DATABASE_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 ALTER TABLE TABLE_NAME CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci; 
